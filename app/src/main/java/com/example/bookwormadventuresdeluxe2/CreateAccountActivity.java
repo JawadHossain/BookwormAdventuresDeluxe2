79618d3a0ef21454dc022cd875c0e1e64f4ce7fe
Hudson <huddy987@gmail.com>
2020-10-15 17:48:45 -0600

    Add Create Account Activity
    Ticket: https://cmput301.atlassian.net/browse/CMPUT-51

--- app/src/main/java/com/example/bookwormadventuresdeluxe2/CreateAccountActivity.java
+++ app/src/main/java/com/example/bookwormadventuresdeluxe2/CreateAccountActivity.java
@@ -2,15 +2,31 @@ package com.example.bookwormadventuresdeluxe2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateAccountActivity extends AppCompatActivity
 {
+    Button createAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        createAccountButton = (Button) findViewById(R.id.createAccountButton2);
+
+        createAccountButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                /* Override login to open Create Account Activity */
+                Intent myBooksIntent = new Intent(CreateAccountActivity.this, MyBooksActivity.class);
+                CreateAccountActivity.this.startActivity(myBooksIntent);
+            }
+        });
     }
 }
