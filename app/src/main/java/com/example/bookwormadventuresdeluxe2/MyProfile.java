02f0b54860af478ceb371be750447579f7617b2e
IsaakC <gcoleman@ualberta.ca>
2020-10-19 22:15:27 -0600

    Very rough version of Requests (GONE UGLY)

--- app/src/main/java/com/example/bookwormadventuresdeluxe2/MyProfile.java
+++ app/src/main/java/com/example/bookwormadventuresdeluxe2/MyProfile.java
@@ -1,7 +1,6 @@
 package com.example.bookwormadventuresdeluxe2;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -12,7 +11,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 
 // Todo: Rename Class to ProfileFragment or rename other fragments
 
@@ -41,7 +39,7 @@ public class MyProfile extends Fragment implements View.OnClickListener
     }
 
     @Override
-    public void onClick(View view) //https://stackoverflow.com/questions/12876624/multiple-edittext-objects-in-alertdialog
+    public void onClick(View view)
     {
         final View editInfo = LayoutInflater.from(this.getContext()).inflate(R.layout.edit_profile, null);
 
