42e34a2faaaa20ba047ff355e319a373bdb78b39
IsaakC <gcoleman@ualberta.ca>
2020-10-25 21:49:25 -0600

    Notification now extends book
    also added createNotification method to book

--- app/src/main/java/com/example/bookwormadventuresdeluxe2/Notification.java
+++ app/src/main/java/com/example/bookwormadventuresdeluxe2/Notification.java
@@ -4,45 +4,19 @@ import android.widget.ImageView;
 
 import com.example.bookwormadventuresdeluxe2.Utilities.Status;
 
-public class Notification
+public class Notification extends Book
 {
-    private Book book;
     private String message;
 
-    public Notification(Book book, String message)
+    public Notification(String title, String author, String description, String isbn, Status status, String message)
     {
-        this.book = book;
+        super(title, author, description, isbn, status);
         this.message = message;
     }
 
     public String getMessage()
     {
         return message;
-    }
-
-    public String getTitle() {
-        return this.book.getTitle();
-    }
-
-    public String getAuthor() {
-        return this.book.getAuthor();
-    }
-
-    public String getIsbn() {
-        return this.book.getIsbn();
-    }
-
-    public Book getBook() {
-        return this.book;
-    }
-
-    // Might have to change later
-    public Status getStatus() {
-        return this.book.getStatus();
-    }
-
-    public void setStatusCircleColor(Status status, ImageView statusCircle) {
-        this.book.setStatusCircleColor(status, statusCircle);
     }
 }
 
